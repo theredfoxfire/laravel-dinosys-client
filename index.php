3254ffe18ea1da5d3f3b10c1842653bbf8157de3
Hasan Setiawan <vizzlearn@gmail.com>
2018-05-03 10:12:51 +0700

    setup dev

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -50,6 +50,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 */
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$app->bind('path.public', function() {
+  return __DIR__;
+});
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Real-time notifications in Laravel using Pusher</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap-notifications.min.css">
+    <link rel="stylesheet" type="text/css" href="{{url('css/bootstrap-notifications.min.css')}}">
 
   </head>
   <body>
